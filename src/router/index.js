cd2d1ec5114a261acd8074205e7db953186755e4
NofathZ <nofath86@gmail.com>
2021-11-16 14:36:39 +0700

    add redirect on all route except for microlaunch

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,12 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../views/Homepage.vue')
+    component: () => import('../views/Homepage.vue'),
   },
   {
     path: '/categories',
     component: () => import('../views/Categories.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/about',
@@ -36,77 +37,92 @@ const routes = [
   {
     path: '/pendaftaran',
     name: 'Registration',
-    component: () => import('../views/Registration.vue')
+    component: () => import('../views/Registration.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/agenda',
     name: 'Agenda',
-    component: () => import('../views/Agenda.vue')
+    component: () => import('../views/Agenda.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/agenda/:slug',
     name: 'Agenda Detail',
-    component: () => import('../views/DetailAgenda.vue')
+    component: () => import('../views/DetailAgenda.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/beasiswa',
     name: 'Scholarship',
-    component: () => import('../views/Scholarship.vue')
+    component: () => import('../views/Scholarship.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/beasiswa/:slug',
     name: 'Scholarship Detail',
-    component: () => import('../views/DetailScholarship.vue')
+    component: () => import('../views/DetailScholarship.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/artikel',
     name: 'Article',
     component: () => import('../views/Article.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/artikel/:slug',
     name: 'Article Detail',
-    component: () => import('../views/DetailArticle.vue')
+    component: () => import('../views/DetailArticle.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/pengumuman',
     name: 'Announcement',
     component: () => import('../views/Announcement.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/pengumuman/:slug',
     name: 'Announcement Detail',
-    component: () => import('../views/DetailAnnouncement.vue')
+    component: () => import('../views/DetailAnnouncement.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/lomba',
     name: 'Competition',
     component: () => import('../views/Competition.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/lomba/:slug',
     name: 'Competition Detail',
-    component: () => import('../views/DetailCompetition.vue')
+    component: () => import('../views/DetailCompetition.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/lowongan',
     name: 'Job Vacancy',
     component: () => import('../views/JobVacancy.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/lowongan/:slug',
     name: 'Job Vacancy Detail',
-    component: () => import('../views/DetailJobVacancy.vue')
+    component: () => import('../views/DetailJobVacancy.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/produk',
     name: 'Product',
-    component: () => import('../views/Product.vue')
+    component: () => import('../views/Product.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/produk/:slug',
     name: 'Product Detail',
-    component: () => import('../views/DetailProduct.vue')
+    component: () => import('../views/DetailProduct.vue'),
+    redirect: '/coming-soon'
   },
   {
     path: '/coming-soon',
